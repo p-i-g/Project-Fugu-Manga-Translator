ede3bdb5a0deeb50d322a4117c6c440b40912cc6
olive <oliverysfx@outlook.com>
2022-03-02 22:48:11 +0800

    remove unnecessary things

--- ocr/utils.py
+++ ocr/utils.py
@@ -45,26 +45,3 @@ class AttnLabelConverter(object):
             text = ''.join([self.character[i] for i in text_index[index, :]])
             texts.append(text)
         return texts
-
-
-class Averager(object):
-    """Compute average for torch.Tensor, used for loss average."""
-
-    def __init__(self):
-        self.reset()
-
-    def add(self, v):
-        count = v.data.numel()
-        v = v.data.sum()
-        self.n_count += count
-        self.sum += v
-
-    def reset(self):
-        self.n_count = 0
-        self.sum = 0
-
-    def val(self):
-        res = 0
-        if self.n_count != 0:
-            res = self.sum / float(self.n_count)
-        return res
