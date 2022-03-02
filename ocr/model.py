d062abe68cb2ea6e1022b29f512d07a9f3fabd03
olive <oliverysfx@outlook.com>
2022-03-02 23:24:06 +0800

    doesn't work

--- ocr/model.py
+++ ocr/model.py
@@ -7,6 +7,7 @@ from feature_extractor import ResNet
 from transformation import TPS_SpatialTransFormerNetwork
 import tensorflow as tf
 from config import Config
+import numpy as np
 class Model(KM.Model):
     def __init__(self, opt):
         super(Model, self).__init__()
@@ -19,19 +20,19 @@ class Model(KM.Model):
         self.AdaptiveAvgPool = KL.GlobalAvgPool2D(data_format='channels_first')  # Transform final (imgH/16-1) -> 1
 
         self.SequenceModeling = keras.Sequential(
-            BidirectionalLSTM(self.FeatureExtraction_output, opt.hidden_size, opt.hidden_size),
-            BidirectionalLSTM(opt.hidden_size, opt.hidden_size, opt.hidden_size))
+            BidirectionalLSTM(self.FeatureExtraction_output, opt.hidden_size),
+            BidirectionalLSTM(opt.hidden_size, opt.hidden_size))
         self.SequenceModeling_output = opt.hidden_size
 
-        self.Prediction = Attention(self.SequenceModeling_output, opt.hidden_size, opt.num_class, opt.num_class, opt.batch_max_length)
+        self.Prediction = Attention(None, self.SequenceModeling_output, opt.hidden_size, opt.num_class, opt.batch_max_length)
 
-    def call(self, input, is_train=True):
+    def call(self, input, is_train=True, **kwargs):
         """ Transformation stage """
         input = self.Transformation(input)
 
         """ Feature extraction stage """
         visual_feature = self.FeatureExtraction(input)
-        visual_feature = self.AdaptiveAvgPool(visual_feature)#.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
+        visual_feature = self.AdaptiveAvgPool(visual_feature[np.newaxis])#.permute(0, 3, 1, 2))  # [b, c, h, w] -> [b, w, c, h]
         visual_feature = tf.squeeze(visual_feature)
 
         """ Sequence modeling stage """
